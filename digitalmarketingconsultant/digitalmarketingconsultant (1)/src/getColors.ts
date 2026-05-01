ba3c50c7cb085b4578a9c92092e5270e8f1dcb58
rahulmukherjee006-max <rahulmukherjee006@gmail.com>
2026-05-02 02:05:47 +0530

    icon changes

--- digitalmarketingconsultant/digitalmarketingconsultant (1)/src/getColors.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-async function getBody() {
-  const res = await fetch('https://cdn.prod.website-files.com/67adaf776f78a657729f252b/css/solvency.webflow.shared.90a82fded.css');
-  const css = await res.text();
-  fs.writeFileSync('./solvency.css', css);
-}
-getBody();
